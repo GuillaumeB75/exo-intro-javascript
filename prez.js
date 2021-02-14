fae834e52a539c4b75b245fd11211b7a3b12a53b
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-02-14 17:01:32 +0100

    corrigé de l'exercice

--- prez.js
+++ prez.js
@@ -1,2 +1,7 @@
-console.log('Hello Hardfork')
-msg = test 
+/* exercice corrigé et renseigné ici */
+let name = 'Sofiane'
+let age = 39
+let city = 'Paris'
+
+let msg = `Hello, my name is ${name} and i am ${age} years old. I live in ${city}.`
+console.log(msg)
